76bad2c33475e2fb506635ef1f64f7c1e47303f6
Decad3nce <decad3nce@gmail.com>
2016-01-06 18:44:43 +0530

    SystemUI: Add LTE Tile
    Change-Id: I16a18532087dba75c65fe3dd5d1b190fb551a7bb
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
    
    Update QS-LTE_tile
    
    Change-Id: Ic393ebcf24f8b9bc1be3a96b8737ac410fc2d30b
    
    remove unused lines
    
    Change-Id: I4a0da49d0b665d56ed774ff22ee3adc0c9ecd1b5

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -39,6 +39,7 @@ public class QSConstants {
 	public static final String TILE_REBOOT = "reboot";
 	public static final String TILE_AMBIENT_DISPLAY = "ambient_display";
 	public static final String TILE_VOLUME = "volume_panel";
+        public static final String TILE_LTE = "lte";
 	public static final String TILE_EDIT = "edit";
 
     protected static final ArrayList<String> STATIC_TILES_AVAILABLE = new ArrayList<String>();
@@ -64,7 +65,7 @@ public class QSConstants {
 		STATIC_TILES_AVAILABLE.add(TILE_REBOOT);
 		STATIC_TILES_AVAILABLE.add(TILE_AMBIENT_DISPLAY);
 		STATIC_TILES_AVAILABLE.add(TILE_VOLUME);
-
+                STATIC_TILES_AVAILABLE.add(TILE_LTE);
         TILES_AVAILABLE.addAll(STATIC_TILES_AVAILABLE);
     }
 }
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -45,6 +45,7 @@ import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.LockscreenToggleTile;
+import com.android.systemui.qs.tiles.LteTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenOffTile;
@@ -64,6 +65,8 @@ import com.android.systemui.statusbar.policy.UserSwitcherController;
 import com.android.systemui.statusbar.policy.ZenModeController;
 import com.android.systemui.tuner.TunerService;
 import com.android.systemui.tuner.TunerService.Tunable;
+import android.telephony.TelephonyManager;
+import com.android.internal.telephony.PhoneConstants;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -168,6 +171,22 @@ public class QSTileHost implements QSTile.Host, Tunable {
         mStatusBar.postStartActivityDismissingKeyguard(intent, 0);
     }
 
+    public static boolean deviceSupportsLte(Context ctx) {
+        final TelephonyManager tm = (TelephonyManager)
+                ctx.getSystemService(Context.TELEPHONY_SERVICE);
+        return (tm.getLteOnCdmaMode() == PhoneConstants.LTE_ON_CDMA_TRUE)
+                || tm.getLteOnGsmMode() != 0;
+    }
+
+    public static boolean deviceSupportsDdsSupported(Context context) {
+        TelephonyManager tm = (TelephonyManager)
+                context.getSystemService(Context.TELEPHONY_SERVICE);
+        return tm.isMultiSimEnabled()
+                && tm.getMultiSimConfiguration() == TelephonyManager.MultiSimVariants.DSDA;
+    }
+
+
+    @Override
     public void startActivityDismissingKeyguard(PendingIntent intent) {
         mStatusBar.postStartActivityDismissingKeyguard(intent);
     }
@@ -301,6 +320,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("reboot")) return  new RebootTile(this);
         else if (tileSpec.equals("ambient_display")) return new AmbientDisplayTile(this);
         else if (tileSpec.equals("volume_panel")) return new VolumeTile(this);
+        else if (tileSpec.equals("lte")) return new LteTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
@@ -369,6 +389,8 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (spec.equals("reboot")) return R.string.quick_settings_reboot_label;
         else if (spec.equals("ambient_display")) return R.string.quick_settings_ambient_display_label;
         else if (spec.equals("volume_panel")) return R.string.quick_settings_volume_panel_label;
+        else if (spec.equals("lte")) return R.string.qs_lte_label;
         return 0;
     }
+
 }
