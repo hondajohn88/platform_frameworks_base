4339d96be72ca0737473a0d7a8a026312f5705e7
Alex Cruz <mazdarider23@gmail.com>
2016-05-13 00:28:58 -0400

    OMS: Fix Quick settings tiles font themes derp [6/6]
    KreAch3R:
    Picked for our substratum related font implementation
    
    This fixes quick settings tiles not changing when you apply a font via
    a theme. I didn't notice this until Randall mentioned it and I tried it
    with GRVY and sure enough.
    
    Anyways, this mirrors the way we do things with the keyguard and every
    other element in the ROM where the font changes.
    
    Change-Id: I03279f2e1cc95fdcb68400ea28d6193df96bff1e

--- packages/SystemUI/src/com/android/systemui/qs/QSTileView.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSTileView.java
@@ -48,8 +48,6 @@ import java.util.Objects;
 
 /** View that represents a standard quick settings tile. **/
 public class QSTileView extends ViewGroup {
-    private static final Typeface CONDENSED = Typeface.create("sans-serif-condensed",
-            Typeface.NORMAL);
 
     private static final String TAG = "QSTileView";
 
@@ -152,7 +150,7 @@ public class QSTileView extends ViewGroup {
             }
             mDualLabel.setTextColor(mContext.getColor(R.color.qs_tile_text));
             mDualLabel.setPadding(0, mDualTileVerticalPaddingPx, 0, mDualTileVerticalPaddingPx);
-            mDualLabel.setTypeface(CONDENSED);
+            mDualLabel.setTypeface(Typeface.create("sans-serif-condensed", Typeface.NORMAL));
             mDualLabel.setTextSize(TypedValue.COMPLEX_UNIT_PX,
                     res.getDimensionPixelSize(R.dimen.qs_tile_text_size));
             mDualLabel.setClickable(true);
@@ -173,7 +171,7 @@ public class QSTileView extends ViewGroup {
             mLabel.setGravity(Gravity.CENTER_HORIZONTAL);
             mLabel.setMinLines(2);
             mLabel.setPadding(0, 0, 0, 0);
-            mLabel.setTypeface(CONDENSED);
+            mLabel.setTypeface(Typeface.create("sans-serif-condensed", Typeface.NORMAL));
             mLabel.setTextSize(TypedValue.COMPLEX_UNIT_PX,
                     res.getDimensionPixelSize(R.dimen.qs_tile_text_size));
             mLabel.setClickable(false);
